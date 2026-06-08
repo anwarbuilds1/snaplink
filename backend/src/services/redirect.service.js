98d71779360d966126a35b1cb9b44f4da4b8509a
Anwar <helloanwar975@gmail.com>
2026-06-09 00:38:57 +0530

    feat: implement global error handling and custom error class

--- backend/src/services/redirect.service.js
+++ backend/src/services/redirect.service.js
@@ -1,10 +1,11 @@
 import * as urlRepository from "../repositories/url.repository.js";
+import AppError from "../utils/AppError.js";
 
 export const getOriginalUrl = async (shortCode) => {
   const url = await urlRepository.findByShortCode(shortCode);
 
   if (!url) {
-    throw new Error("Short URL not found");
+    throw new AppError("Short URL not found", 404);
   }
 
   await urlRepository.incrementClickCount(url._id);
